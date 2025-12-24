6eb58d5956f27713f7e5e2a9f6a9a2fa8abdc3ad
pavelsur07 <pavelsur07@gmail.com>
2025-12-24 10:02:56 +0300

    Add academy link to main menu

--- wp-content/themes/vashfindir/header.php
+++ wp-content/themes/vashfindir/header.php
@@ -34,6 +34,7 @@
                 <a class="vf-nav__link" href="<?php echo esc_url(home_url('/')); ?>">Главная</a>
                 <a class="vf-nav__link" href="<?php echo esc_url(home_url('/service/')); ?>">Сервис</a>
                 <a class="vf-nav__link" href="<?php echo esc_url(home_url('/service-findir/')); ?>">Сервис + Финдир</a>
+                <a class="vf-nav__link" href="<?php echo esc_url(home_url('/academy-finance/')); ?>">Академия</a>
                 <a class="vf-nav__link" href="<?php echo esc_url(home_url('/about/')); ?>">О нас</a>
             </nav>
 
@@ -63,6 +64,7 @@
                     <a class="vf-mobile__link" href="<?php echo esc_url(home_url('/')); ?>">Главная</a>
                     <a class="vf-mobile__link" href="<?php echo esc_url(home_url('/service/')); ?>">Сервис</a>
                     <a class="vf-mobile__link" href="<?php echo esc_url(home_url('/service-findir/')); ?>">Сервис + Финдир</a>
+                    <a class="vf-mobile__link" href="<?php echo esc_url(home_url('/academy-finance/')); ?>">Академия</a>
                     <a class="vf-mobile__link" href="<?php echo esc_url(home_url('/about/')); ?>">О нас</a>
                 </div>
 
