ce580d7f7d0f0be59c9d016228120b06aced7606
pavelsur07 <pavelsur07@gmail.com>
2026-01-10 14:29:51 +0300

    Add vf-surface utility and apply to landing cards

--- wp-content/themes/vashfindir/template-parts/landing/faq.php
+++ wp-content/themes/vashfindir/template-parts/landing/faq.php
@@ -12,21 +12,21 @@ if (!defined('ABSPATH')) {
         </header>
 
         <div class="vf-faq__body">
-            <details class="vf-faq__item">
+            <details class="vf-faq__item vf-surface">
                 <summary class="vf-faq__question">Сколько времени занимает старт проекта?</summary>
                 <div class="vf-faq__answer">
                     <p>Обычно первые результаты видны в течение 2–3 недель после старта: мы настраиваем сбор данных, согласуем метрики и запускаем регулярную отчётность.</p>
                 </div>
             </details>
 
-            <details class="vf-faq__item">
+            <details class="vf-faq__item vf-surface">
                 <summary class="vf-faq__question">Кто будет работать с моим бизнесом?</summary>
                 <div class="vf-faq__answer">
                     <p>Команда включает старшего CFO и аналитика. Они погружаются в специфику бизнеса и ведут коммуникацию на регулярных встречах.</p>
                 </div>
             </details>
 
-            <details class="vf-faq__item">
+            <details class="vf-faq__item vf-surface">
                 <summary class="vf-faq__question">Можно ли подключиться на тестовый период?</summary>
                 <div class="vf-faq__answer">
                     <p>Да, мы начинаем с пилотного периода, чтобы вы оценили качество аналитики и удобство процессов без долгосрочных обязательств.</p>
