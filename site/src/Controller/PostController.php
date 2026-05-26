da7ccb2bd4518a507420fa5c19be5b7cf57dcc4e
Pavel Novikov <pavelsur07@gmail.com>
2026-05-26 10:31:18 +0300

    refactoring style #1

--- site/src/Controller/PostController.php
+++ site/src/Controller/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
-
+    #[Route('/gazeta/post-1', name: 'gazeta_post_1')]
+    public function privacy(): Response
+    {
+        return $this->render('blog/post/post.html.twig');
+    }
 }
